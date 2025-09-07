ac469c809ae801eec7647180cb81d1621cea355f
Vladislav Melenchuk <v.melenchuk03@gmail.com>
2025-09-07 16:48:44 +0300

    add HW_8

---
+++ HW_8/HW_mvc_3/HW_mvc_3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HW_mvc_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HW_8/HW_mvc_3/HW_mvc_3/Program.cs
@@ -0,0 +1,29 @@
+using HW_mvc_3.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=AdminPanel}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
