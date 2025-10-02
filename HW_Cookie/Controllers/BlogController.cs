2dca80d8157da529dc7d566421a14c942d7ce4bb
Vladislav <10328026@stud.op.edu.ua>
2025-10-02 04:18:08 +0300

    add HW

---
+++ HW_Cookie/Controllers/BlogController.cs
@@ -0,0 +1,41 @@
+﻿using HW_Cookie.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW_Cookie.Controllers
+{
+    public class BlogController : Controller
+    {
+        private static List<News> _newsList = new List<News>
+        {
+            new News { Id = 1, Title = "Первая новость", Content = "Содержимое первой новости", Date = DateTime.Now },
+            new News { Id = 2, Title = "Вторая новость", Content = "", Date = DateTime.Now.AddDays(-1) }
+        };
+
+        public IActionResult Index()
+        {
+            var theme = Request.Cookies["theme"] ?? "light";
+            ViewBag.Theme = theme;
+
+            return View(_newsList);
+        }
+
+        [HttpGet]
+        public IActionResult Settings()
+        {
+            var theme = Request.Cookies["theme"] ?? "light";
+            ViewBag.Theme = theme;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Settings(string theme)
+        {
+            Response.Cookies.Append("theme", theme, new CookieOptions
+            {
+                Expires = DateTimeOffset.UtcNow.AddDays(30)
+            });
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HW_Cookie/Models/News.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_Cookie.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Content { get; set; } = "";
+        public DateTime Date { get; set; }
+    }
+}
