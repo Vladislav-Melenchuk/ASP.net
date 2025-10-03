22b43ab652b428ef92e702debe655456243e4eb9
Vladislav <10328026@stud.op.edu.ua>
2025-10-03 15:33:10 +0300

    add HW

---
+++ HW_RazorPage/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using HW_RazorPage.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace HW_RazorPage.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoItem> Todos => Set<TodoItem>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
---
+++ HW_RazorPage/Program.cs
@@ -0,0 +1,37 @@
+using HW_RazorPage.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Todo}/{action=Index}/{id?}");
+
+app.Run();
